37e638925dd17d5983f5728a069da7e2ab4612e7
Brian Muturi <muturibrian18@gmail.com>
2020-12-22 01:45:59 +0100

    Add project files.

---
+++ quadratic equation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace quadratic_equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y, a, b, c, x;
+            Console.Write("a:" );
+            a = double.Parse(Console.ReadLine());
+            Console.Write("b:" );
+            b = double.Parse(Console.ReadLine());
+            Console.Write("x:" );
+            x = double.Parse(Console.ReadLine());
+            Console.Write("c:");
+            c = double.Parse(Console.ReadLine());
+            y = (a * (x * x)) + (b * x) + c;
+            Console.Write("y="  + y);
+            
+        }
+    }
+}
